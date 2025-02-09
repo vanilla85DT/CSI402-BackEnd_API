1d2640c2f7b5354774178e18cd021a28f8c2f421
85VNL <93180438+vanilla85DT@users.noreply.github.com>
2025-02-09 14:37:20 +0700

    fin

---
+++ basicNextJs.md
@@ -0,0 +1,50 @@
+ 1 สร้างโปรเจกต์ใหม่
+
+    1.1 npx create-next-app@latest ecommerce-app --typescript
+    1.2 cd ecommerce-app
+
+ 2 ติดตั้ง Dependencies ที่จำเป็นตามความชอบ
+    
+   npm install @prisma/client bcrypt zustand axios @mui/material @emotion/react @emotion/styled @heroicons/react @stripe/stripe-js @stripe/react-stripe-js
+   npm install -D prisma tailwindcss postcss autoprefixer
+
+   ### หน้าที่ของแต่ละตัว 
+   Frontend: React (zustand, @mui/material, @heroicons/react, @emotion/react, TailwindCSS) เพื่อสร้าง UI และจัดการสถานะ
+   Backend: Prisma, bcrypt, Stripe, และ axios เพื่อเชื่อมต่อฐานข้อมูล, เข้ารหัสข้อมูล, และรองรับการชำระเงิน
+   CSS:TailwindCSS ทำงานร่วมกับ PostCSS และ Autoprefixer เพื่อประมวลผล CSS อย่างมีประสิทธิภาพ
+
+3 คำสั่งgit
+
+  3.1 เชื่อมต่อกับ Remote Repository ใช้  git remote add origin https://github.com/vanilla85DT/testNexttttttt.git
+
+  3.2 ส่งข้อมูลไปยัง Remote Repository ใช้ git push origin <branch>
+
+  3.3 ดึงข้อมูลจาก Remote Repository ใช้ git pull origin <branch>
+
+  การทำงานกับ Branch
+     
+      1. สร้าง Branch ใหม่ใช้      git branch <branch-name>
+      2. ย้ายไปยัง Branch อื่นใช้    git checkout <branch-name>
+
+  คำสั่งเกี่ยวกับ file 
+      
+   1. เพิ่มไฟล์ใน Staging Area 
+      
+      เพิ่มไฟล์เฉพาะไฟล์เดียว ใช้ git add <filename>
+      เพิ่มทุกไฟล์ที่เปลี่ยนแปลง  ใช้ git add .
+
+   2.บันทึกการเปลี่ยนแปลง (Commit) 
+
+      git commit -m "ข้อความอธิบายการเปลี่ยนแปลง"
+
+
+
+git remote remove origin
+git remote -v
+
+git init
+git add .
+git commit -m "wahahaha"
+git remote add origin https://github.com/vanilla85DT/CSI402-Frontend-NextJS.git
+git push origin main
+git push origin main --force
--- src/app.ts
+++ src/app.ts
@@ -12,33 +12,47 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-app.post("/registration/tempAcount" , (req:Request,res:Response) =>{
-    const {firstName,lastName,idCard,birthDay,email} = req.body
-    
-    if (!firstName || !lastName || !idCard || !birthDay || !email) {
-        res.status(400).json({
-            status:"400",
-             msg:"pls chk rqs bd" 
-        })
-    } else {
-        res.status(200).json({
-            status:"200",
-            msg:"ok",
-            data:{
-                username:"TA000210342",
-                psd:"00023A",
-                email:"ta000210342@taiwan.com",
-            }
-        })
-    }
-});
+interface RedeemRequest {
+    studentId: string;
+    amount: number;
+}
 
-app.post("/screen-code", (req: Request, res: Response) => {
-    // Implement the logic for the screen code functionality here
-    res.status(200).json({
-        status: "200",
-        msg: "Screen code endpoint is working",
-    });
+const initialPoints = 1000; //แต้มที่จารกำหนดให้
+
+app.post("/spumem/points", (req: Request, res: Response) => {
+    try {
+        const { studentId, amount }: RedeemRequest = req.body;
+
+        if (!studentId || amount === undefined) {
+            return res.status(400).json({ status: "400", msg: "ข้อมูลไม่ครบถ้วน" });
+        }
+
+        //เงินมากกว่า 0 เป็นจำนวนเต็ม
+        if (!Number.isInteger(amount) || amount <= 0) {
+            return res.status(400).json({ status: "400", msg: "จำนวนเงินไม่ถูกต้อง" });
+        }
+
+        //นับแต้ม
+        const earnPoints = Math.floor(amount / 100) * 10;
+        const totalPoints = initialPoints + earnPoints;
+
+        return res.status(200).json({
+            status: "200",
+            msg: "สะสมแต้มสำเร็จแล้ว",
+            data: {
+                studentId,
+                earnPoints,
+                totalPoints,
+            },
+        });
+
+    } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).json({
+            status: "500",
+            msg: "เกิดข้อผิดพลาด",
+        });
+    }
 });
 
 app.listen(3000, () => {
